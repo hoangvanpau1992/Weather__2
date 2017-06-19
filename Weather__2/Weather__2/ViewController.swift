bc37e65b16d7b3421b9ab797ca1c7ca95bb3da22
DucPT <ducft.nb@gmail.com>
2017-06-19 10:17:48 +0700

    Commit

---
+++ Weather__2/Weather__2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather__2
+//
+//  Created by admin on 6/19/17.
+//  Copyright © 2017 Apple lnc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
